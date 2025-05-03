fb45793000e4621e7f36d14ca4053a51ff5ca0a2
nikol <77391893+nikol-reukr@users.noreply.github.com>
2025-05-03 15:43:16 +0300

    Flake8 corrections

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 class Person:
     people = {}
-    def __init__(self, name: str, age: int):
+
+    def __init__(self, name: str, age: int) -> None:
+
         self.name = name
         self.age = age
         Person.people[self.name] = self
-
 
 
 def create_person_list(people: list) -> list:
